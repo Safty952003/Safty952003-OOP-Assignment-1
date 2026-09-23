3b061db3057ad5572c564b1560bfc9ea744bed98
Safty952003 <safty952003@gmail.com>
2026-09-23 03:24:03 +0300

    feat: add OrderLine class

---
+++ Part1_ProceduralToOOP/src/OrderLine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Part1_ProceduralToOOP;
+
+public class OrderLine
+{
+    public Product Product { get; }
+    public int Quantity { get; }
+
+    public OrderLine(Product product, int quantity)
+    {
+        if (quantity <= 0)
+            throw new ArgumentException("Quantity must be positive.");
+
+        Product = product;
+        Quantity = quantity;
+    }
+
+    public double CalculateTotal()
+    {
+        return Product.Price * Quantity;
+    }
+}
